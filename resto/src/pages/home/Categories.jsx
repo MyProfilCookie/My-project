78422b23575df0bceaee6f39a21d800dbdc385d7
MyProfilCookie <parigopsg24@gmail.com>
2024-04-09 17:47:26 +0200

    mise à jour de la page home avec des ajustement au niveau du scss

--- resto/src/pages/home/Categories.jsx
+++ resto/src/pages/home/Categories.jsx
@@ -18,12 +18,12 @@ function Categories() {
 
             </div>
 
-            <div className='flex flex-wrap gap-8 md-flex-col justify-around items-center mt-12'>
+            <div className='flex flex-wrap gap-4 md-flex-col justify-around items-center mt-12'>
                 {
                     Category.map((category) => {
-                        return <div key={category.id} className=' shadow-lg rounded-md py-6 px-8 w-50 bg-white rounded-md mx-auto text-center cursor-pointer' >
+                        return <div key={category.id} className=' shadow-lg rounded-md py-6 w-64 bg-white rounded-md mx-auto text-center cursor-pointer' >
                             <div className='w-64 mx-auto'>
-                                <img className='rounded-md w-64' src={category.image} alt={category.name} />
+                                <img className='w-64' src={category.image} alt={category.name} />
                             </div>
                             <div className='mt-5 space-y-1 '>
                                 <h5 className='text-center font-bold'>{category.name}</h5>
