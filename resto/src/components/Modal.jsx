cb19c364ec82a813279a8743520e8112368cdd8e
MyProfilCookie <parigopsg24@gmail.com>
2024-04-12 20:46:42 +0200

    correction et ajout du modal

--- resto/src/components/Modal.jsx
+++ resto/src/components/Modal.jsx
@@ -5,6 +5,10 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import {faCakeCandles} from '@fortawesome/free-solid-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'react-router-dom'
 
 function Modal() {
@@ -28,7 +32,7 @@ function Modal() {
                 <div className="modal-overlay">
                     <div className="modal">
                         <div className="modal-box">
-                            <div className="modal-action">
+                            <div className="modal-action flex flex-col justify-center mt-0">
                                 <form className="card-body" method='dialog'>
                                     <h3 className="text-center font-bold "><FontAwesomeIcon icon={faCakeCandles} className='text-red hover-scale-110 transition duration-300' /> Se connecter</h3>
                                     {/* <button onClick={closeModal} className="btn btn-close">Close</button> */}
@@ -54,10 +58,21 @@ function Modal() {
                                         <p className="text-center">Vous n’avez pas de compte ? <Link to='/register' className='underline text-red font-bold mt-2'>S’inscrire</Link></p>
                                     </div>
                                 </form>
-                                <div>
-                                     <button onClick={closeModal} className="butn btn-circle btn-outline "><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                <div className='flex justify-center mx-4'>
+                                    <button className="butn btn-circle btn-outline-google ">
+                                    <FontAwesomeIcon icon={faGoogle} size='xl' />    
+                                    </button>
+                                    <button className="butn btn-circle btn-outline-git ">
+                                    <FontAwesomeIcon icon={faGithubAlt} size='2xl' />    
+                                    </button>
+                                    <button className="butn btn-circle btn-outline-facebook ">
+                                    <FontAwesomeIcon icon={faFacebook} size='2xl' />   
+                                    </button>
+                                    <button className="butn btn-circle btn-outline-twitter ">
+                                    <FontAwesomeIcon icon={faXTwitter} size='2xl' />    
                                     </button>
                                 </div>
+
                             </div>
                         </div>
                     </div>
